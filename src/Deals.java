c2a2adef106ac6dc4a2c8435734b22ecd4a7f44a
GirishRiggy <giri.riggy@gmail.com>
2016-06-04 17:49:35 +0530

    Added new code base for git test

---
+++ src/Deals.java
@@ -0,0 +1,55 @@
+
+public class Deals {
+
+	String transport;
+	String departure;
+	String arrival;
+	Object duration;
+	long cost;
+	double discount;
+	String reference;
+	
+	public String getTransport() {
+		return transport;
+	}
+	public void setTransport(String transport) {
+		this.transport = transport;
+	}
+	public String getDeparture() {
+		return departure;
+	}
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+	public String getArrival() {
+		return arrival;
+	}
+	public void setArrival(String arrival) {
+		this.arrival = arrival;
+	}
+	public Object getDuration() {
+		return duration;
+	}
+	public void setDuration(Object duration) {
+		this.duration = duration;
+	}
+	public long getCost() {
+		return cost;
+	}
+	public void setCost(long cost) {
+		this.cost = cost;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+		
+}
---
+++ src/Main.java
@@ -0,0 +1,57 @@
+
+public class Main {
+
+//	public List<Deals> getDeals() {
+//
+//		ArrayList<Deals> allDeals = new ArrayList<Deals>();
+//		ObjectMapper mapper = new ObjectMapper();
+//
+//		Deals deal = new Deals();
+//
+//		mapper.readValue(new File("D:\\response.json"), Deals.class);
+//
+//		
+//		return allDeals;
+//	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Lets create nodes as given as an example in the article
+		Node nA = new Node('A');
+		Node nB = new Node('B');
+		Node nC = new Node('C');
+		Node nD = new Node('D');
+		Node nE = new Node('E');
+		Node nF = new Node('F');
+
+		// Create the graph, add nodes, create edges between nodes
+		Graph g = new Graph();
+		g.addNode(nA);
+		g.addNode(nB);
+		g.addNode(nC);
+		g.addNode(nD);
+		g.addNode(nE);
+		g.addNode(nF);
+		g.setRootNode(nA);
+
+		g.connectNode(nA, nB);
+		g.connectNode(nA, nC);
+		g.connectNode(nA, nD);
+
+		g.connectNode(nB, nE);
+		g.connectNode(nB, nF);
+		g.connectNode(nC, nF);
+
+		// Perform the traversal of the graph
+		System.out.println("DFS Traversal of a tree is ------------->");
+		g.dfs();
+
+		System.out.println("\nBFS Traversal of a tree is ------------->");
+		g.bfs();
+
+	}
+
+}
